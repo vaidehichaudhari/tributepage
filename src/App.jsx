b59dbcaf0d04f18a20ba908c00e8c0e5b7f3e1e2
Vaidehi <vaidehic48@gmail.com>
2025-03-23 17:02:45 +0530

    Info added

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import HeaderRS from './components/HeaderRS.jsx'
 import headerimage from './assets/rs.jpeg'
 import Achievment from './components/Achievment.jsx'
 import ImageGallary from './components/ImageGallary.jsx'
+import Footer from './components/Footer.jsx'
+import Info from './components/Info.jsx'
 function App() {
 
   return (
     <>
-      <HeaderRS/>
-      <img src={headerimage} alt="image not showing" className="imageclass"/>
-<Achievment/>
-<ImageGallary/>
+      <HeaderRS />
+     
+      <img src={headerimage} alt="image not showing" className="imageclass" />
+      <Info />
+      <Achievment />
+      <ImageGallary />
+
+      <Footer />
     </>
   )
 }
---
+++ src/components/Info.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div>
+        <h2>Profile</h2>
+      <p><b>Rohit Sharma</b> (born 30 April 1987) is an Indian international cricketer and the captain of Indian cricket team in Test and ODI formats. He is widely regarded as one of the greatest ODI opening batters of all time.[3] He is a right-handed batsman who plays for Mumbai Indians in Indian Premier League and for Mumbai in domestic cricket. Rohit previously captained India in all three formats. After leading India to the victory at the 2024 Men's T20 World Cup, he announced his retirement from T20Is.[4][5] He also led India to victory in the 2025 ICC Champions Trophy.</p>
+   <p>He holds several batting records which famously include most runs in T20 Internationals, most sixes in international cricket,[a] most double centuries in ODI cricket (3), most centuries at Cricket World Cups (7) and joint most hundreds in Twenty20 Internationals (5)[7].He also holds the world record for the highest individual score (264) in a One Day International (ODI) and also holds the record for scoring most hundreds (five) in a single Cricket World Cup, for which he won the ICC Men's ODI Cricketer of the Year award in 2019.[8] He is the first and only captain to lead a team in all[b] ICC tournament finals.[9</p>
+    </div>
+  )
+}
+
+export default Info
